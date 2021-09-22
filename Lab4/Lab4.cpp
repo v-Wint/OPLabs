9df8e7ce43c7454024a08d6e03c95d5e657d2ff6
v-Wint <veselyjburunduk@gmail.com>
2021-09-22 16:52:44 +0300

    Completed lab 4

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double getH(double a, double b, int n) {
+	return (b - a) / n;
+}
+
+int main()
+{
+	double a, b, h;
+	int n;
+
+	// data input
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "n = "; cin >> n;
+	
+	if (n == 0) {
+		cout << "Sorry, but n cannot equal 0";
+		return 0;
+	}
+
+	h = getH(a, b, n);
+
+	for (int i = 1; i <= n; i++) {
+		printf("%i - %.3f\n", i, a + i * h);
+	}
+
+	return 0;
+}
--- lab_4/lab_4.py
+++ lab_4/lab_4.py
@@ -5,18 +5,18 @@ def get_h(a, b, n):
 def main():
     try:
         a, b, n = float(input("a = ")), float(input("b = ")), int(input("n = "))
-
-        if n == 0:
-            print("Sorry, but n cannot be 0")
-            return
     except:
         print("Oh-oh, it seems the data you entered is invalid ^_^")
+        return
+
+    if n == 0:
+        print("Sorry, but n cannot equal 0")
         return
 
     h = get_h(a, b, n)
 
     for i in range(1, n + 1):
         result = a + i*h
-        print(f"{i} — {round(result, 5)}")
+        print(f"{i} — {round(result, 3)}")
 
 main()
