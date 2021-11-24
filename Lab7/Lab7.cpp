9fe43687b79e3815f97cbf70fd14a99e04b95d41
v-Wint <veselyjburunduk@gmail.com>
2021-11-24 09:20:00 +0200

    - minor changes

--- Lab7/Lab7.cpp
+++ Lab7/Lab7.cpp
@@ -11,30 +11,38 @@ double* initialize(size_t);    //initialize dynamic array with the random float 
 
 void display(double*, size_t);  //display elements of a given array
 
-void proccess(double* arr, size_t size, int z, int& numberOfElements, double& maxElement, int& maxElementIndex);  //finds the number of elements smaller than z, the greatest of those elements and its index
+void proccessElementsSmallerThan(double* arr, size_t size, int z, int& numberOfElements, int& maxElementIndex, double& maxElement);  //finds the number of elements smaller than z, the greatest of those elements and its index
 
 void replaceElementWithFirst(double*, size_t, double); // replace the given element with the first in the array
 
 int main() {
-    size_t n;
-    int z;
+    // values input
+    size_t n; int z;
     cout << "n = "; cin >> n;
     cout << "z = "; cin >> z;
+    if (n < 1 ) {
+        printf("n cannot be smaller than 1\n"); 
+        return 0;
+    }
 
+    //array initialization
     double* B = initialize(n);
 
     printf("Initial array: \n"); display(B, n);
 
-    int numberOfElements = 0, maxElementIndex;
-    double maxElement = -RANGE;
+    //process the elements smaller than z
+    int numberOfElements, maxElementIndex; double maxElement;
 
-    proccess(B, n, z, numberOfElements, maxElement, maxElementIndex);
+    proccessElementsSmallerThan(B, n, z, numberOfElements, maxElementIndex, maxElement);
 
-    if (numberOfElements == 0)
+    // if there are such elements, output, replace the greatest of those with the first
+    if (numberOfElements == 0) {
         printf("There are no elements smaller than %d in the array\n", z);
+    }
     else {
         printf("There are %d elements smaller than %d. The greatest of those is the element %f with the index %d\n\n", \
             numberOfElements, z, maxElement, maxElementIndex);
+
         replaceElementWithFirst(B, maxElementIndex, maxElement);
 
         printf("Array after replacement:\n"); display(B, n);
@@ -60,19 +68,26 @@ void display(double* arr, size_t size) {
     cout << "\n\n";
 }
 
-void proccess(double* arr, size_t size, int z, int& numberOfElements, double& maxElement, int& maxElementIndex) {
+void proccessElementsSmallerThan(double* arr, size_t size, int z, int& numberOfElements, int& maxElementIndex, double& maxElement) {
     double el;
+    numberOfElements = 0, maxElementIndex = -1;
     for (size_t i = 0; i < size; i++) {
         el = arr[i];
         if (el < z) {
+            
             numberOfElements++;
-            if (el > maxElement) {
-                maxElement = el;
+
+            if (maxElementIndex < 0 ) {  //if the first occurence and we can`t know the maxElement
                 maxElementIndex = i;
+                maxElement = el;
+            }
+            else if (el > maxElement) { 
+                maxElementIndex = i;
+                maxElement = el;
             }
         }
-
     }
+    
 }
 void replaceElementWithFirst(double* array, size_t index, double value) {
     double firstElement = array[0];
